0f1ba0422a458c17119ba1acc80b5d1b5ef8bdbf
Lok <hka2@inbox.lv>
2024-04-21 16:26:40 +0300

    Added initial version of Estates view, updated navigation bar: added search button and estate button

--- RealEstate1/Program.cs
+++ RealEstate1/Program.cs
@@ -30,7 +30,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Estates}/{action=Index}/{id?}");
 
 AppDbInitializer.Seed(app);
 
